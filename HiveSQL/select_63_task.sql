d580244db7b8456aaba9bba7e62c17442e37a514
iyuriy <ibocharov@172.18.129.161>
2018-10-08 11:54:55 +0300

    fix

--- HiveSQL/select_63_task.sql
+++ HiveSQL/select_63_task.sql
@@ -1,7 +1,14 @@
+-- using UDF (change according your jar-name)
+delete jar /home/cloudera/my/b-0.0.1.jar;
+add jar /home/cloudera/my/b-0.0.1.jar;
+create temporary function getStartIP as 'a.udf.GetStartIP';
+create temporary function getEndIP as 'a.udf.GetEndIP';
+create temporary function getIP as 'a.udf.GetIP';
+
 -- create view Product with ip as long
 create view if not exists vproduct as 
 select tb.ip, getIP(tb.ip) as iip, tb.p as sump
-from (SELECT t.ip, sum(t.price) p FROM product t group by t.ip order by p desc limit 10) tb;
+from (SELECT t.ip, sum(t.price) sump FROM product t group by t.ip order by sump desc limit 10) tb;
 
 -- create view CountryIP with startip as long and endip as long
 create view if not exists vcountryip as 
@@ -12,4 +19,23 @@ from countryip tb;
 SELECT t2.sump, t3.geoname_id, t4.country_name FROM vproduct t2, vcountryip t3 
 INNER JOIN countryName t4 ON t4.geoname_id = t3.geoname_id 
 WHERE t2.iip < t3.endip AND t2.iip > t3.startip LIMIT 10;
+
+
+
+
+-- test
+
+SELECT t2.sump, t3.geoname_id FROM vproduct t2, vcountryip t3 
+WHERE t2.iip < t3.endip AND t2.iip > t3.startip LIMIT 10;
+
+SELECT t2.sump, t3.geoname_id FROM vproduct t2, 
+(select tb.geoname_id, getStartIP(tb.network) as startip, getEndIP(tb.network) as endip
+from countryip tb) t3  WHERE t2.iip < t3.endip AND t2.iip > t3.startip LIMIT 10;
+
+SELECT t2.sump, t3.geoname_id FROM
+ (select tb.ip, getIP(tb.ip) as iip, tb.p as sump
+  from (SELECT t.ip, sum(t.price) p FROM product t group by t.ip order by p desc limit 10) tb limit 10) t2, 
+ (select tb.geoname_id, getStartIP(tb.network) as startip, getEndIP(tb.network) as endip
+  from countryip tb) t3  
+ WHERE t2.iip < t3.endip AND t2.iip > t3.startip LIMIT 10;
 
--- HiveSQL/udf.sql
+++ HiveSQL/udf.sql
@@ -1,6 +1,6 @@
 -- using UDF (change according your jar-name)
-delete jar /home/cloudera/my/b-0.0.1.jar
-add jar /home/cloudera/my/b-0.0.1.jar
-create temporary function getStartIP as 'a.udf.GetStartIP'
-create temporary function getEndIP as 'a.udf.GetEndIP'
-create temporary function getIP as 'a.udf.GetIP'
+delete jar /home/cloudera/my/b-0.0.1.jar;
+add jar /home/cloudera/my/b-0.0.1.jar;
+create temporary function getStartIP as 'a.udf.GetStartIP';
+create temporary function getEndIP as 'a.udf.GetEndIP';
+create temporary function getIP as 'a.udf.GetIP';
