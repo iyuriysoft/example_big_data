225b63481c9c97f3613ae93d58e93def5182cc66
iyuriy <ibocharov@172.18.150.48>
2018-10-09 20:31:09 +0300

    fixed

--- HiveSQL/select_51_52_tasks.sql
+++ HiveSQL/select_51_52_tasks.sql
@@ -1,12 +1,18 @@
 
 -- 5.1
+DROP TABLE IF EXISTS table51;
+CREATE TABLE table51 AS
 SELECT category, COUNT(*) as cnt FROM product GROUP BY category ORDER BY cnt DESC LIMIT 10;
 
 -- 5.2
+DROP TABLE IF EXISTS table52;
+CREATE TABLE table52 AS
 SELECT tp.name, tp.category, count(*) as cnt FROM product tp inner join
 (select category, count(*) as c from product group by category order by c desc) tcat
 ON tp.category = tcat.category
 GROUP BY tp.name, tp.category ORDER BY cnt DESC LIMIT 10;
 
 -- 6.3 show ip only
+DROP TABLE IF EXISTS table63ip;
+CREATE TABLE table63ip AS
 SELECT t.ip, sum(t.price) sump FROM product t GROUP BY t.ip ORDER BY sump DESC LIMIT 10;
--- HiveSQL/select_63_task.sql
+++ HiveSQL/select_63_task.sql
@@ -16,6 +16,8 @@ select tb.geoname_id, getStartIP(tb.network) as startip, getEndIP(tb.network) as
 from countryip tb;
 
 -- 6.3 task: show country name
+DROP TABLE IF EXISTS table63;
+CREATE TABLE table63 AS
 SELECT t2.sump, t3.geoname_id, t4.country_name FROM vproduct t2, vcountryip t3 
 INNER JOIN countryName t4 ON t4.geoname_id = t3.geoname_id 
 WHERE t2.iip < t3.endip AND t2.iip > t3.startip LIMIT 10;
